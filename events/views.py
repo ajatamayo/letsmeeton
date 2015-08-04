dcabeab6772f754fcb5b5605e458726af0963294
Ariel Tamayo <ajatamayo@gmail.com>
2015-08-04 20:14:47 +0800

    Add fonts

--- events/views.py
+++ events/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
 
+from .models import Event
+
 
 class HomepageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomepageView, self).get_context_data(**kwargs)
+        try:
+            event = Event.objects.latest('id')
+        except Event.DoesNotExist:
+            event = None
+
+        context['event'] = event
+        return context
